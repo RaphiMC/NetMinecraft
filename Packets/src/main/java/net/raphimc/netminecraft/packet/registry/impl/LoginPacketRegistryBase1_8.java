cd5edc4e141444c198bd35fe998ed0c0f5433354
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2022-09-30 22:43:24 +0200

    Fixed 1.7.x support

--- Packets/src/main/java/net/raphimc/netminecraft/packet/registry/impl/LoginPacketRegistryBase1_8.java
+++ Packets/src/main/java/net/raphimc/netminecraft/packet/registry/impl/LoginPacketRegistryBase1_8.java
@@ -1,12 +1,15 @@
 package net.raphimc.netminecraft.packet.registry.impl;
 
-import net.raphimc.netminecraft.packet.impl.login.S2CLoginCompressionPacket;
+import net.raphimc.netminecraft.packet.impl.login.*;
 
 public class LoginPacketRegistryBase1_8 extends LoginPacketRegistryBase1_7_6 {
 
     public LoginPacketRegistryBase1_8(boolean clientside) {
         super(clientside);
 
+        this.registerC2SPacket(0x01, C2SLoginKeyPacket1_8::new);
+
+        this.registerS2CPacket(0x01, S2CLoginKeyPacket1_8::new);
         this.registerS2CPacket(0x03, S2CLoginCompressionPacket::new);
     }
 
