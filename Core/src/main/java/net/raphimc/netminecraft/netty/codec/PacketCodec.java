14ae11ddc980a5af60616214883e26a783cedf5a
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-07-01 00:36:42 +0200

    Updated build.gradle, Relicensed under LGPL-3.0

--- Core/src/main/java/net/raphimc/netminecraft/netty/codec/PacketCodec.java
+++ Core/src/main/java/net/raphimc/netminecraft/netty/codec/PacketCodec.java
@@ -1,10 +1,29 @@
+/*
+ * This file is part of NetMinecraft - https://github.com/RaphiMC/NetMinecraft
+ * Copyright (C) 2023 RK_01/RaphiMC and contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.raphimc.netminecraft.netty.codec;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageCodec;
 import net.raphimc.netminecraft.constants.MCPipeline;
-import net.raphimc.netminecraft.packet.*;
+import net.raphimc.netminecraft.packet.IPacket;
+import net.raphimc.netminecraft.packet.PacketTypes;
+import net.raphimc.netminecraft.packet.UnknownPacket;
 
 import java.lang.reflect.Field;
 import java.util.List;
--- Core/src/main/java/net/raphimc/netminecraft/util/ServerAddress.java
+++ Core/src/main/java/net/raphimc/netminecraft/util/ServerAddress.java
@@ -1,6 +1,25 @@
+/*
+ * This file is part of NetMinecraft - https://github.com/RaphiMC/NetMinecraft
+ * Copyright (C) 2023 RK_01/RaphiMC and contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.raphimc.netminecraft.util;
 
-import javax.naming.directory.*;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
 import java.net.IDN;
 import java.net.InetSocketAddress;
 import java.util.Hashtable;
